05f4207e01a56762b25757f167285ec76dc08da1
Andrés <>
2018-09-07 14:28:20 -0500

    Esto es para que comience a trabajar como el video y para Ventocilla

--- src/Controlador/DadoThread.java
+++
@@ -1,7 +0,0 @@
-
-package Controlador;
-
-public class DadoThread extends Thread {
-    
-    
-}
---
+++ src/Modelos/Panel.java
@@ -0,0 +1,16 @@
+
+package Modelos;
+import java.awt.*;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+public class Panel extends JPanel{
+    ArrayList v;
+    public Panel(ArrayList asd)
+    {
+        this.v=asd;
+    }
+    public void paint()
+    {
+        
+    }
+}
